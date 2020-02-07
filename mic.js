cb9576b9b65e6e4dcce5b630bce85431a3ae89e6
skang04 <35582459+skang04@users.noreply.github.com>
2020-02-07 14:50:50 -0800

    Add files via upload

--- mic.js
+++ mic.js
@@ -1,26 +1,47 @@
 let mic;
 let cnv;
+let sensitive = 7;
+let happy;
+let sad;
+let background;
 
 function setup() {
-    cnv = createCanvas(1000, 1000);
-    centerCanvas();
+    cnv = createCanvas(1, 1);
+    cnv.position(100, 1);
     mic = new p5.AudioIn();
     mic.start();
-}
-
-function centerCanvas() {
-    let x = (windowWidth - width) / 2;
-    let y = (windowHeight - height) / 2;
-    cnv.position(x, y)
+    sad = select('.sad');
+    happy = select('.happy');
+    background = select('body');
 }
 
 function draw() {
     getAudioContext().resume();
-    background(255);
-    var vol = (mic.getLevel() * 3000);
-    noStroke();
-    ellipse(width / 2, height / 2, 300, 300);
-    fill(255, 221 - vol, 110)
-    console.log(vol);
+    let vol = (mic.getLevel() * 3000);
 
+    if (vol > 20) {
+        sensitive = sensitive + 2;
+    } else {
+        sensitive = sensitive - 1;
+    }
+
+    if (sensitive < 10) {
+        sensitive = 10;
+    } else if (sensitive > 350) {
+        sensitive = 350;
+    }
+
+
+    if (sensitive > 100) {
+        happy.style('display', 'block');
+        sad.style('display', 'none');
+        background.style('background-color', '#90D7EC')
+
+    } else {
+        happy.style('display', 'none');
+        sad.style('display', 'block')
+        background.style('background-color', '#FFDE94')
+    }
+
+    console.log(sensitive);
 }
